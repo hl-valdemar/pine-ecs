d91011462c0430807cc63aecb0b14ab8b3e95c3a
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-06-17 11:21:34 +0200

    Updated build script to dynamically build examples

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
 
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -29,30 +29,84 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const exe_examples_mod = b.createModule(.{
-        .root_source_file = b.path("src/examples/example.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // Build all files names in the src/examples folder
+    const examples_path = "src/examples/";
+    var dir = try std.fs.cwd().openDir(examples_path, .{ .iterate = true });
+    var it = dir.iterate();
+    while (try it.next()) |file| {
+        if (file.kind != .file) {
+            continue;
+        }
 
-    exe_examples_mod.addImport("pecs", lib_mod);
+        std.debug.print("[building...] {s}{s}\n", .{ examples_path, file.name });
 
-    const exe_examples = b.addExecutable(.{
-        .name = "example",
-        .root_module = exe_examples_mod,
-    });
+        const allocator = std.heap.page_allocator;
+        const full_path = std.fmt.allocPrint(allocator, "{s}{s}", .{ examples_path, file.name }) catch "format failed";
+        defer allocator.free(full_path);
 
-    b.installArtifact(exe_examples);
+        const exe_mod = b.createModule(.{
+            .root_source_file = b.path(full_path),
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const run_cmd_examples = b.addRunArtifact(exe_examples);
-    run_cmd_examples.step.dependOn(b.getInstallStep());
+        exe_mod.addImport("pecs", lib_mod);
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd_examples.addArgs(args);
+        var words = std.mem.splitAny(u8, file.name, ".");
+        const example_name = words.next().?;
+
+        // This creates another `std.Build.Step.Compile`, but this one builds an executable
+        // rather than a static library.
+        const exe = b.addExecutable(.{
+            .name = example_name,
+            .root_module = exe_mod,
+        });
+
+        // Add src/lib for libraries.
+        // exe.addIncludePath(.{
+        //     .src_path = .{ 
+        //         .owner = b,
+        //         .sub_path = "src/lib",
+        //     },
+        // });
+
+        b.installArtifact(exe);
+
+        // This *creates* a Run step in the build graph, to be executed when another
+        // step is evaluated that depends on it. The next line below will establish
+        // such a dependency.
+        const run_cmd = b.addRunArtifact(exe);
+
+        // By making the run step depend on the install step, it will be run from the
+        // installation directory rather than directly from within the cache directory.
+        // This is not necessary, however, if the application depends on other installed
+        // files, this ensures they will be present and in the expected location.
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step_desc = std.fmt.allocPrint(allocator, "Run {s} example", .{ example_name }) catch "format failed";
+        defer allocator.free(run_step_desc);
+
+        // This creates a build step. It will be visible in the `zig build --help` menu,
+        // and can be selected like this: `zig build run`
+        // This will evaluate the `run` step rather than the default, which is "install".
+        const run_step = b.step(example_name, run_step_desc);
+        run_step.dependOn(&run_cmd.step);
+
+        // const exe_unit_tests = b.addTest(.{
+        //     .root_module = exe_mod,
+        // });
+        //
+        // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        //
+        // // Similar to creating the run step earlier, this exposes a `test` step to
+        // // the `zig build --help` menu, providing a way for the user to request
+        // // running the unit tests.
+        // test_step.dependOn(&run_exe_unit_tests.step);
     }
-
-    const run_step_examples = b.step("example", "Run the given example");
-    run_step_examples.dependOn(&run_cmd_examples.step);
 }
